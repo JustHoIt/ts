fb6e913050366d73438fda2d5014b03b2627d113
Just_Ho_it <zalop3128@kakao.com>
2025-08-20 23:53:37 +0900

    Any부터 Intersection까지

--- src/main.ts
+++ src/main.ts
@@ -65,13 +65,61 @@ const hello: () => void = function () {
 const h : void = hello()
 
 /// Any
+//아무거나 할당 가능 그러나, 되도록 사용 X
+let hello2: any = 'Hello world'
+hello2 = 123
+hello2 = false
+hello2 = null
+hello2 = {}
+hello2 = []
+hello2 = function() {}
 
 /// Unknown
+const a1: any = 123
+const u: unknown = 123
+
+const any: any = a1
+const boo: boolean = a1
+const num2: number = a1
+const arr2: string[] = a1
+const obj2: { x: string, y: number } = a1
 
 /// Tuple
+const tuple: [string, number, boolean] = ['a', 1, false]
+const users: [number, string, boolean] []
+= [[1, "A", true], [2, "B", false], [3, "C", true],]
 
 /// Void
+function hello3(msg: string): void {
+    console.log(`Hello' ${msg}`)
+}
+
+const hi: void = hello3('world')
+
+/// Never
+const nev: [] = []
 
 /// Union
+let union2: string | number | number [] | boolean
+union2 = 'Hello type!'
+union2 = 1
+union2 = [1,2,3]
+union2 = false
+
+let union3: (string | number | number [] | boolean) []
+union3 = ['Hello type!', 1, [1,2,3], false]
 
 /// Intersection
+interface User3 {
+    name: string,
+    age: number
+}
+interface Validation {
+    isValid: boolean
+}
+
+const ho: User & Validation = {
+    name: 'homin',
+    age: 222,
+    isValid: true
+}
